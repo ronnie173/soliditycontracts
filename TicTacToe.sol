e2155417e7e9c506285f4772699edf1592ea751c
Jerome Raymond <ronnie173@gmail.com>
2018-04-16 21:38:34 -0500

    Added more funtionality

--- TicTacToe.sol
+++ TicTacToe.sol
@@ -7,10 +7,12 @@ contract TicTacToe {
     address[3][3] board;
     address public player1;
     address public player2;
-    address activePlayer;
+    address activePLyer;
+    event PlayerJoined(address player,string message);
+
 
 constructor()
-public {
+public{
 player1 = msg.sender;
 }
 
@@ -20,11 +22,12 @@ assert(player2 == address(0));
 gameActive = true;
 player2 = msg.sender;
 activePLyer = player2;
+emit PlayerJoined(activePLyer,"Player has joined");
 }
 
 function getBoard()
 public
-view returns (address[3][3]){
+view returns(address[3][3]){
 return board;
 }
 
@@ -42,6 +45,7 @@ gameActive = false;
 
 function setStone(uint8 x, uint8 y)
 public {
+
 require(board[x][y] == address(0));
 assert(gameActive);
 assert(x < boardSize);
@@ -49,63 +53,68 @@ assert(y < boardSize);
 require(msg.sender == activePLyer);
 board[x][y] = msg.sender;
 movesCounter++;
-for(uint8 i = 0; i < boardSize; i++){
-if (board[i][y] != activePLyer){
+for(uint8 i =0; i < boardSize; i++){
+if(board[i][y] != activePLyer){
 break;
 }
 //win
-if (i == boardSize - 1){
+if(i == boardSize -1){
 //winner
 setWinner(activePLyer);
+return;
 }
 }
 
-for (i = 0; i < boardSize; i++){
-if (board[x][i] != activePLyer){
+for(i =0; i < boardSize; i++){
+if(board[x][i] != activePLyer){
 break;
 }
 //win
-if (i == boardSize - 1){
+if(i == boardSize -1){
 //winner
 setWinner(activePLyer);
+return;
+
 }
 }
 //check to see if diagonal
-if (x == y){
-for (i = 0; i < boardSize; i++){
-if (board[i][i] != activePLyer){
+if(x == y){
+for(i =0; i < boardSize; i++){
+if(board[i][i] != activePLyer){
 break;
 }
 //win
-if (i == boardSize - 1){
+if(i == boardSize -1){
 //winner
 setWinner(activePLyer);
-
+return;
 }
 }
 }
 
 //check for anti diagonal
-if ((x + y) == boardSize -1){
-for (i = 0; i < boardSize; i++){
-if (board[i][(boardSize -1) - i] != activePLyer){
+if((x+y) == boardSize -1){
+for(i =0; i < boardSize; i++){
+if(board[i][(boardSize -1) -i] != activePLyer){
 break;
 }
 //win
-if (i == boardSize - 1){
+if(i == boardSize -1){
 //winner
 setWinner(activePLyer);
+return;
 }
 }
 }
 
 //draw
-if (movesCounter == (boardSize ** 2)){
+if(movesCounter == (boardSize**2)){
 //a draw
 setDraw();
+return;
 }
 
-if (activePLyer == player1){
+if(activePLyer == player1){
 activePLyer = player2;
 }else{
 activePLyer = player1;
